52d5240f731a5f004d8c4ae578d4ead5d519e15b
Johnny <johnrobertstewart@protonmail.com>
2022-09-09 15:58:07 -0700

    might f up dependancies

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -3,6 +3,8 @@ import {FaBars, FaTimes} from 'react-icons/fa'
 
 const NavBar = () => {
 
+  const [nav, setNan] = useState(false);
+
   const links = [
   {
     id: 1,
@@ -14,11 +16,11 @@ const NavBar = () => {
   },
   {
     id: 3,
-    link: 'about'
+    link: 'portfolio'
   },
   {
     id: 4,
-    link: 'about'
+    link: 'contact'
   },
 ]
   return (
@@ -28,7 +30,7 @@ const NavBar = () => {
         <h1 className="text-5xl font-signature ml-2"> Jor </h1>
     </div>
 
-      <ul className="flex">
+      <ul className="hidden md:flex">
       {links.map(({id, link}) => (
         <li key = {id} className="px-4 cursor-pointer capitalize font-medium
         text-grey-500 hover:500 hover:scale-105 duration 200">
@@ -36,6 +38,26 @@ const NavBar = () => {
           </li>
       ))}      
       </ul>
+
+      <div onClick={() => setNav(!nav)} 
+      className="cursor-pointer pr-4 z-10 text-gray-500">
+      
+      {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+        {nav && (
+
+
+      
+    <ul className="flex flex-col justify center items-center
+    absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black
+    to-gray-800 text-grey-500">
+      {links.map(({id, link}) => (
+      <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl">
+        {link}
+        </li>
+      ))}
+    </ul>
+        )}
   </div>
   );
 };
