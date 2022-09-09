51aa754a26938e7544bcb96a419ff99d2078c13c
John Stewart <johnrobertstewart@protonmail.com>
2022-09-09 11:52:06 -0700

    Changed it all

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import NavBar from './assets/components/NavBar';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar>  </NavBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+const NavBar = () => {
+  return (
+  <div class="flex justify-between items-center w-full h-20 
+ px-4 text-white bg-black fixed">
+    <div>
+        <h1 className="text-5xl font-signature ml-2">
+            Jor
+        </h1>
+    </div>
+
+  </div>)
+}
+
+export default NavBar
